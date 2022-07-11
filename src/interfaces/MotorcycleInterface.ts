3dd0a18384e406c4c913883ccf5dc0953570c1f4
max <maxmullermiranda@gmail.com>
2022-07-11 02:46:59 -0300

    feat: Motorcycle interface

---
+++ src/interfaces/MotorcycleInterface.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+import { Vehicle } from './VehicleInterface';
+
+const VALUES = ['Street', 'Custom', 'Trail'] as const;
+
+const MotorcycleSchema = z.object({
+  category: z.enum(VALUES),
+  engineCapacity: z.number().int().min(1).max(2500),
+});
+
+export type Motorcycle = z.infer<typeof MotorcycleSchema> & Vehicle;
+export { MotorcycleSchema };
